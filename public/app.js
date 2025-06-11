9be8c593c99a2cd00c6cee8f4122af0d4052d6ce
Grand Sage InExtremis <grand.sage.inextremis@gmail.com>
2025-06-11 14:31:58 +0200

    feat: add the CSS style

--- public/app.js
+++ public/app.js
@@ -12,11 +12,6 @@ function runApplication(data)
 		let subjectData;
 		let subjectTitle;
 		let sectionList;
-		let sectionListItem;
-		let sectionTitle;
-		let courseNoteList;
-		let courseNoteListItem;
-		let courseNoteAnchor;
 		
 		subjectData = data.subjects.filter(subject => (subject.id === subjectID))[0];
 
@@ -34,27 +29,48 @@ function runApplication(data)
 		sectionList = document.getElementsByClassName('sectionList')[0];
 		sectionList.textContent = '';
 
-		for (const section of subjectData.sections)
+		for (let i = 0; i < subjectData.sections.length; i++)
 		{
+			let section = subjectData.sections[i];
+
+			let sectionListItem;
+			let sectionTitle;
+			let courseNoteList;
+
 			sectionListItem = document.createElement('li');
-			sectionListItem.classList.add('sectionList__section');
 			sectionListItem.classList.add('section');
 
 			sectionTitle = document.createElement('h2');
 			sectionTitle.classList.add('section__title');
-			sectionTitle.textContent = section.title;
+			sectionTitle.textContent = `${i+1}.  ${section.title}`;
 			sectionListItem.appendChild(sectionTitle);
 
 			courseNoteList = document.createElement('ol');
 			courseNoteList.classList.add('section__courseNoteList');
 
-			for (const courseNote of section.courseNotes)
+			for (let j = 0; j < section.courseNotes.length; j++)
 			{
+				let courseNote = section.courseNotes[j];
+
+				let courseNoteListItem;
+				let courseNoteAnchor;
+				let courseNoteTitle;
+				let courseNoteNo;
+
+				courseNoteNo = document.createElement('span');
+				courseNoteNo.classList.add('section__courseNoteNo');
+				courseNoteNo.textContent = `${i+1}.${j+1}.`;
+
+				courseNoteTitle = document.createElement('span');
+				courseNoteTitle.classList.add('section__courseNoteTitle');
+				courseNoteTitle.textContent = courseNote.title;
+
 				courseNoteAnchor = document.createElement('a');
 				courseNoteAnchor.classList.add('section__courseNoteLink');
-				courseNoteAnchor.textContent = courseNote.title;
 				courseNoteAnchor.setAttribute('href', `/courseNotes/${courseNote.id}.pdf`);
 				courseNoteAnchor.setAttribute('target', `_blank`);
+				courseNoteAnchor.appendChild(courseNoteNo);
+				courseNoteAnchor.appendChild(courseNoteTitle);
 
 				courseNoteListItem = document.createElement('li');
 				courseNoteListItem.classList.add('section__courseNote')
