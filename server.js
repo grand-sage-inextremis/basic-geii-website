9b3cc9899b72a3536069a706f9d7c54ba930baab
Grand Sage InExtremis <grand.sage.inextremis@gmail.com>
2025-06-15 13:16:23 +0200

    fix: rename '/public' to '/docs'
    When using GitHub Pages, the public directory is '/docs'.

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ import express from 'express';
 
 const app = express();
 
-app.use(express.static('./public'));
+app.use(express.static('./docs'));
 
 app.listen(8000, 'localhost', () => console.log("Server running on http://localhost:8000"));
